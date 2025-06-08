6da560329bc1736c5357e02855d15aa24fbd0ab5
schigor <igor_sch@interia.eu>
2025-06-08 15:09:27 +0200

    app db

---
+++ Ciezarki/MVVM/Model/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ciezarki.MVVM.Model
+{
+    internal class AppDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+    }
+}

01da2c4e04c79a722a84c033f87e7e4b86aafa63
unknown <sommerantek@gmail.com>
2025-06-09 14:54:17 +0200

    style

---
+++ Ciezarki/Core/TextBoxHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Ciezarki.Core
+{
+    public static class TextBoxHelper
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.RegisterAttached(
+                "Title",
+                typeof(string),
+                typeof(TextBoxHelper),
+                new FrameworkPropertyMetadata(string.Empty));
+
+        public static void SetTitle(UIElement element, string value)
+        {
+            element.SetValue(TitleProperty, value);
+        }
+
+        public static string GetTitle(UIElement element)
+        {
+            return (string)element.GetValue(TitleProperty);
+        }
+
+        public static readonly DependencyProperty BorderWidthProperty =
+            DependencyProperty.RegisterAttached(
+                "BorderWidth",
+                typeof(double),
+                typeof(TextBoxHelper),
+                new FrameworkPropertyMetadata(330.0));
+
+        public static void SetBorderWidth(UIElement element, double value)
+        {
+            element.SetValue(BorderWidthProperty, value);
+        }
+
+        public static double GetBorderWidth(UIElement element)
+        {
+            return (double)element.GetValue(BorderWidthProperty);
+        }
+
+        public static readonly DependencyProperty BorderHeightProperty =
+    DependencyProperty.RegisterAttached(
+        "BorderHeight",
+        typeof(double),
+        typeof(TextBoxHelper),
+        new FrameworkPropertyMetadata(100.0));
+
+        public static void SetBorderHeight(UIElement element, double value)
+        {
+            element.SetValue(BorderHeightProperty, value);
+        }
+
+        public static double GetBorderHeight(UIElement element)
+        {
+            return (double)element.GetValue(BorderHeightProperty);
+        }
+    }
+}

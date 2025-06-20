b7df551a78f07111fdd45ad1df49a6800800a885
schigor <igor_sch@interia.eu>
2025-06-20 11:34:57 +0200

    add exercise vm

--- Ciezarki/MVVM/Viewmodel/AddExerciseVM.cs
+++ Ciezarki/MVVM/Viewmodel/AddExerciseVM.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Ciezarki.MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Ciezarki.MVVM.Viewmodel
 {
@@ -10,7 +13,64 @@ namespace Ciezarki.MVVM.Viewmodel
     {
         public AddExerciseVM()
         {
+            _exercise = new Exercise();
+        }
 
+        private Exercise _exercise;
+
+        public String Name
+        {
+            get => _exercise.Name;
+            set
+            {
+                _exercise.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public String Description
+        {
+            get => _exercise.Description;
+            set
+            {
+                _exercise.Description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+     public string Muscle
+        {
+            get => _exercise.Muscle;
+            set
+            {
+                _exercise.Muscle = value;
+                OnPropertyChanged(nameof(Muscle));
+            }
+        }
+
+        public ICommand SaveExerciseCommand => new RelayCommand(_=>Save());
+
+        public void Save()
+        {
+            if (string.IsNullOrEmpty(Muscle) || string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Description))
+            {
+                MessageBox.Show("Należy wypełnić wszystkie pola przed dodaniem ćwiczenia!", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                using (var context = new AppDbContext())
+                {
+                    if (context.Exercises.Any(e => e.Name == Name))
+                    {
+                        MessageBox.Show("Ćwiczenie o tej nazwie już istnieje!", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    context.Exercises.Add(_exercise);
+                    context.SaveChanges();
+                }
+                MessageBox.Show("Ćwiczenie zostało dodane pomyślnie!", "Sukces!", MessageBoxButton.OK, MessageBoxImage.Information);
+               
+            }
         }
     }
 }
